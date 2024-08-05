00ea71bae47a240ffec0550b9d442c32efd10433
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-08-05 21:16:28 +0530

    Update Program4.py

--- Program4.py
+++ Program4.py
@@ -1,1 +1,14 @@
+Write a program to blur and smoothing an image
+-----------------------------------------------------------
+import cv2 
+# Read the image 
+28 
+image = cv2.imread('images.jpg') 
+# Apply Gaussian blur 
+blurred_image = cv2.GaussianBlur(image, (15, 15), 0) 
+# Display the original and blurred images 
+cv2.imshow('Original Image', image) 
+cv2.imshow('Blurred Image', blurred_image) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
 
