e5baf722e277f899300d60d27a091a86d4317215
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-08-06 10:07:25 +0530

    Update Program5.py

--- Program5.py
+++ Program5.py
@@ -3,14 +3,18 @@ Write a program to contour an image
 import cv2 
 # Read the image in grayscale 
 image = cv2.imread('images.jpg', cv2.IMREAD_GRAYSCALE) 
+
 # Apply binary thresholding 
 ret, thresh = cv2.threshold(image, 127, 255, 0) 
+
 # Find contours 
 contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, 
 cv2.CHAIN_APPROX_SIMPLE) 
+
 # Draw contours on a blank image 
 contour_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR) 
-cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2) 
+cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
+
 # Display the original and contoured images 
 cv2.imshow('Original Image', image) 
 cv2.imshow('Contoured Image', contour_image) 
